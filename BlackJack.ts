b9e57a08063ec4f0ce68168a2ae82c39f067c5a9
Brian Ocampos <brianmatias999@gmail.com>
2024-11-29 20:47:40 -0300

    Subiendo archivos,se modifico el main borrando el contenido y en blackjack se cambio parte del codigo en las primeras 10 lineas

--- BlackJack.ts
+++ BlackJack.ts
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-import { Apuesta } from "./interfaz";
-import { Juego } from "./juego";
-class BlackJack extends Juego implements Apuesta {
-    private apuestaActual: number;
-    private saldoGanado: number;
-
-    constructor(nombre: string, tipoDeJuego: string, premio: number) {
-        super(nombre, tipoDeJuego, premio);
-        this.apuestaActual = 0;
-        this.saldoGanado = 0;
-    }
-
-    dineroGanado(): number {
-        return this.apuestaActual * this.saldoGanado;
-    }
-
-    dineroPerdido(): number {
-        return this.apuestaActual - this.saldoGanado;
-    }
-
-    apuestaMinima(): number {
-        if (this.apuestaActual < 100) {
-            console.log(`Saldo insuficiente para realizar la apuesta.`);
-        } else {
-            console.log(`Apuesta realizada con ${this.apuestaActual} puntos.`);
-        }
-        return this.apuestaActual;
-    }
-    apuestaMaxima(): number {
-        throw new Error("Method not implemented.");
-    }
-
-    // Métodos de la Interfaz Apuesta
-    realizarApuesta(monto: number): void {
-        if (monto < this.apuestaActual) {
-            this.apuestaActual = monto;
-            this.saldoGanado = this.apuestaActual;
-        } else {
-            this.saldoGanado = 0;
-            console.log(
-                `El jugador ha perdido ${monto - this.apuestaActual} puntos.`
-            );
-        }
-    }
-
-    //Metodos de la interfaz Juego
-    getPremio(): number {
-        return this.premio;
-    }
-    getEstado(): string {
-        return this.estado;
-    }
-    getNombre(): string {
-        return this.nombre;
-    }
-    getTipoDeJuego(): string {
-        return this.tipoDeJuego;
-    }
-    instruccionJuego(): void {
-        console.log(
-            `Instrucciones del juego "${this.nombre}": Este es un juego de tipo "${this.tipoDeJuego}". Sigue las reglas si deseas ganar el premio de ${this.premio} puntos.`
-        );
-    }
-}
-=======
 import { Juego } from "./Juego";
 import { Apuesta } from "./Interfaz";
 
@@ -155,4 +89,3 @@ class BlackJack extends Juego implements Apuesta {
 
 export { BlackJack };
 
->>>>>>> 333e960759c257432e718fc405c3444b53b12ed4
