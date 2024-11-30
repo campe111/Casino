bfe3bdfa2d660863196739926b69516c81606372
ThomasEcheverria2024 <thomasdiegoecheverria@gmail.com>
2024-11-29 22:08:35 -0300

    Subiendo Bingo

--- Bingo.js
+++ Bingo.js
@@ -1,81 +1,73 @@
-class BingoGame {
-    private carton: number[] = [];
-    private bolasLlamadas: number[] = [];
-    private bolasMarcadas: number[] = [];
-
-    constructor() {
+var BingoGame = /** @class */ (function () {
+    function BingoGame() {
+        this.carton = [];
+        this.bolasLlamadas = [];
+        this.bolasMarcadas = [];
         this.carton = this.generarCarton();
         this.bolasLlamadas = this.generarBolas();
     }
-
     // Genera el cartón de bingo (15 números entre 1 y 90)
-    private generarCarton(): number[] {
-        const carton: number[] = [];
+    BingoGame.prototype.generarCarton = function () {
+        var carton = [];
         while (carton.length < 15) {
-            const numero = Math.floor(Math.random() * 90) + 1;
+            var numero = Math.floor(Math.random() * 90) + 1;
             if (!carton.includes(numero)) {
                 carton.push(numero);
             }
         }
-        carton.sort((a, b) => a - b);
+        carton.sort(function (a, b) { return a - b; });
         return carton;
-    }
-
+    };
     // Genera las bolas sorteadas (90 bolas posibles)
-    private generarBolas(): number[] {
-        const bolas: number[] = [];
+    BingoGame.prototype.generarBolas = function () {
+        var bolas = [];
         while (bolas.length < 90) {
-            const bola = Math.floor(Math.random() * 90) + 1;
+            var bola = Math.floor(Math.random() * 90) + 1;
             if (!bolas.includes(bola)) {
                 bolas.push(bola);
             }
         }
         return bolas;
-    }
-
+    };
     // Función para mostrar el cartón con los números marcados
-    private mostrarCarton(): void {
-        const cartonMarcado = this.carton.map(num =>
-            this.bolasMarcadas.includes(num) ? `${num} (Marcado)` : `${num}`
-        );
+    BingoGame.prototype.mostrarCarton = function () {
+        var _this = this;
+        var cartonMarcado = this.carton.map(function (num) {
+            return _this.bolasMarcadas.includes(num) ? "".concat(num, " (Marcado)") : "".concat(num);
+        });
         console.table("Cartón de Bingo:");
         console.table(cartonMarcado.join(' '));
-    }
-
+    };
     // Función principal del juego
-    public jugar(): void {
+    BingoGame.prototype.jugar = function () {
         console.log("¡Comienza el juego de Bingo!");
-
-        let bingo = false;
+        var bingo = false;
         this.mostrarCarton();
-
-        for (const bola of this.bolasLlamadas) {
-            console.log(`Bola llamada: ${bola}`);
-
+        for (var _i = 0, _a = this.bolasLlamadas; _i < _a.length; _i++) {
+            var bola = _a[_i];
+            console.log("Bola llamada: ".concat(bola));
             // Si la bola está en el cartón, la marcamos
             if (this.carton.includes(bola)) {
                 this.bolasMarcadas.push(bola);
-                console.log(`¡Marcado! Número: ${bola}`);
+                console.log("\u00A1Marcado! N\u00FAmero: ".concat(bola));
             }
-
             // Mostrar el estado del cartón
             this.mostrarCarton();
-
             // Verificar si se ha alcanzado el Bingo
             if (this.bolasMarcadas.length === this.carton.length) {
                 bingo = true;
                 break;
             }
         }
-
         if (bingo) {
             console.log("¡Bingo! Has marcado todos los números.");
-        } else {
+        }
+        else {
             console.log("El juego ha terminado, pero no hay Bingo.");
         }
-    }
-}
-
+    };
+    return BingoGame;
+}());
 // Crear una instancia del juego y ejecutarlo
-const juego = new BingoGame();
+var juego = new BingoGame();
 juego.jugar();
