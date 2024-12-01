dac24cc8dd4f25464fc9c24e495004fe2e7a2e7b
Wanda Hernandez <hernandezswanda@hotmail.com>
2024-12-01 20:07:08 -0300

    Recuperando archivo main

---
+++ main.ts
@@ -0,0 +1,42 @@
+import * as readline from "readline";
+import { BlackJack } from "./BlackJack";
+import { Bingo } from "./Bingo";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function mostrarMenu(): void {
+    console.log("--- Casino King Of Coins ---");
+    console.log("1. Jugar al blackjack");
+    console.log("2. Jugar al bingo");
+    console.log("3. Salir");
+    const bingo = new Bingo();
+    const blackJack = new BlackJack("Blackjack", "BlackJack", 1000, 100);
+    rl.question("Seleccione una opcion: ", (opcion) => {
+        switch (opcion) {
+            case "1":
+                blackJack.iniciarJuego();
+                blackJack.repartirCartas(2);
+                blackJack.plantarse();
+                blackJack.calcularSumaDeCartas();
+                break;
+
+        }
+        volverAlMenuPrincipal();
+    });
+}
+// Función para volver al menú principal
+function volverAlMenuPrincipal(): void {
+    rl.question("\n¿Desea volver al menú principal? (si/no): ", (respuesta) => {
+        if (respuesta.toLowerCase() === "si") {
+            mostrarMenu();
+        } else {
+            console.log("¡Gracias por jugar!");
+            rl.close();
+        }
+    });
+}
+mostrarMenu();
+volverAlMenuPrincipal();
